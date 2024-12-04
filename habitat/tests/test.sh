e940dcc739fc8d723afc95a4b5363ea9a9018dd9
Sachin <sachin.jain@chef.io>
2024-12-04 12:21:36 +0530

    add pipleline for build, publish
    Signed-off-by: Sachin <sachin.jain@chef.io>

---
+++ habitat/tests/test.sh
@@ -0,0 +1,23 @@
+set -euo pipefail
+
+
+project_root="$(git rev-parse --show-toplevel)"
+
+# print error message followed by usage and exit
+error () {
+  local message="$1"
+
+  echo -e "\nERROR: ${message}\n" >&2
+
+  exit 1
+}
+
+[[ -n "$pkg_ident" ]] || error 'no hab package identity provided'
+
+package_version=$(awk -F / '{print $3}' <<<"$pkg_ident")
+
+cd "${project_root}"
+
+echo "--- :msg_right: Testing ${pkg_ident} executables"
+actual_version=$(hab pkg exec "${pkg_ident}" fauxhai -v | sed -E 's/.*: ([0-9]+\.[0-9]+\.[0-9]+).*/\1/')
+[[ "$package_version" = "$actual_version" ]] || error "Fauxhai version is not the expected version. Expected '$package_version', got '$actual_version'"
