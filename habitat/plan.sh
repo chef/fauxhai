b8a410cb09a263e3b9c66a027750512103f93d47
Sachin <sachin.jain@chef.io>
2024-12-04 12:29:18 +0530

    fix habitat build for linux
    Signed-off-by: Sachin <sachin.jain@chef.io>

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,84 +1,74 @@
+export HAB_BLDR_CHANNEL="LTS-2024"
 pkg_name=fauxhai
-pkg_origin=core
+pkg_origin=chef
+ruby_pkg="core/ruby3_1"
 pkg_description="Easily mock full ohai data"
-pkg_license=('Apache-2.0')
-pkg_deps=(
-  core/ruby31
-  core/bash
-)
+pkg_deps=(${ruby_pkg} core/coreutils)
 pkg_build_deps=(
-  core/gcc
-  core/make
-)
+    core/make
+    core/sed
+    core/gcc
+    core/libarchive
+    )
 pkg_bin_dirs=(bin)
+do_setup_environment() {
+  build_line 'Setting GEM_HOME="$pkg_prefix/vendor"'
+  export GEM_HOME="$pkg_prefix/vendor"
+
+  build_line "Setting GEM_PATH=$GEM_HOME"
+  export GEM_PATH="$GEM_HOME"
+}
 
 pkg_version() {
   cat "$SRC_PATH/VERSION"
 }
-
 do_before() {
   update_pkg_version
 }
-
-# Setup environment variables for Ruby Gems
-do_setup_environment() {
-  build_line "Setting up GEM_HOME and GEM_PATH"
-  export GEM_HOME="$pkg_prefix/lib"
-  export GEM_PATH="$GEM_HOME"
-}
-
-# Unpack the source files into the cache directory
 do_unpack() {
-  local unpack_dir="$HAB_CACHE_SRC_PATH/$pkg_dirname"
-  build_line "Creating unpack directory: $unpack_dir"
-  mkdir -pv "$unpack_dir"
-  cp -RT "$PLAN_CONTEXT"/.. "$unpack_dir/"
+  mkdir -pv "$HAB_CACHE_SRC_PATH/$pkg_dirname"
+  cp -RT "$PLAN_CONTEXT"/.. "$HAB_CACHE_SRC_PATH/$pkg_dirname/"
 }
-
-# Build the gem from the gemspec file
 do_build() {
-  build_line "Building the gem from the gemspec file"
-  pushd "$HAB_CACHE_SRC_PATH/$pkg_dirname" > /dev/null
-  gem build fauxhai-chef.gemspec
-  popd > /dev/null
-}
 
-# Install the built gem into the package directory
+    export GEM_HOME="$pkg_prefix/vendor"
+
+    build_line "Setting GEM_PATH=$GEM_HOME"
+    export GEM_PATH="$GEM_HOME"
+    bundle config --local without integration deploy maintenance
+    bundle config --local jobs 4
+    bundle config --local retry 5
+    bundle config --local silence_root_warning 1
+    bundle install
+    gem build fauxhai-chef.gemspec
+}
 do_install() {
-  build_line "Installing the gem"
-  pushd "$HAB_CACHE_SRC_PATH/$pkg_dirname" > /dev/null
+   export GEM_HOME="$pkg_prefix/vendor"
+
+  build_line "Setting GEM_PATH=$GEM_HOME"
+  export GEM_PATH="$GEM_HOME"
   gem install fauxhai-*.gem --no-document
-  popd > /dev/null
-
-  wrap_fauxhai_bin
+  set_runtime_env "GEM_PATH" "${pkg_prefix}/vendor"
+  wrap_ruby_bin
 }
-
-# Create a wrapper script to properly set paths and execute the fauxhai command
-wrap_fauxhai_bin() {
+wrap_ruby_bin() {
   local bin="$pkg_prefix/bin/$pkg_name"
   local real_bin="$GEM_HOME/gems/fauxhai-chef-${pkg_version}/bin/fauxhai"
-
-  build_line "Creating wrapper script: $bin"
+  build_line "Adding wrapper $bin to $real_bin"
   cat <<EOF > "$bin"
 #!$(pkg_path_for core/bash)/bin/bash
 set -e
-
-# Set the PATH for Fauxhai to include necessary binaries
+# Set binary path that allows InSpec to use non-Hab pkg binaries
 export PATH="/sbin:/usr/sbin:/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:\$PATH"
-
 # Set Ruby paths defined from 'do_setup_environment()'
-export GEM_HOME="$GEM_HOME"
-export GEM_PATH="$GEM_PATH"
-
-# Execute the Fauxhai binary
-exec $(pkg_path_for core/ruby31)/bin/ruby $real_bin "\$@"
+  export GEM_HOME="$pkg_prefix/vendor"
+  export GEM_PATH="$GEM_PATH"
+exec $(pkg_path_for ${ruby_pkg})/bin/ruby $real_bin \$@
 EOF
-
-  # Ensure the wrapper script is executable
   chmod -v 755 "$bin"
 }
 
-# No additional stripping needed
+
 do_strip() {
   return 0
 }
