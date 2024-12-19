595d0ec74400d193d34866ed96491ae3a30df2b4
nitin sanghi <nsanghi@progress.com>
2024-12-19 11:30:03 +0530

    Experditor trigger for promoting pkg
    Signed-off-by: nitin sanghi <nsanghi@progress.com>

---
+++ .expeditor/promote-hab-pkg.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -eou pipefail
+
+HAB_AUTH_TOKEN=$(vault kv get -field auth_token account/static/habitat/chef-ci)
+export HAB_AUTH_TOKEN
+
+source_channel="workstation-build"
+ hab pkg promote "${EXPEDITOR_PKG_IDENT}" "${EXPEDITOR_TARGET_CHANNEL}"  "${EXPEDITOR_PKG_TARGET}"
