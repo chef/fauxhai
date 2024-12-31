2100e164dc37c767d1890a2811d4d8b24de3d379
Sean Simmons <sean.simmons@progress.com>
2024-12-30 19:05:25 -0500

    updtaing workflow for expeditor config
    also updating script to promote from STABLE to workstation-LTS
    
    Signed-off-by: Sean Simmons <sean.simmons@progress.com>

--- .expeditor/promote-hab-pkg.sh
+++ .expeditor/promote-hab-pkg.sh
@@ -1,10 +1,45 @@
 #!/bin/bash
 
+#
+# The following standard Expeditor artifact_actions environment variables
+# are automatically set by the calling process:
+#
+# PROMOTABLE - in promotion artifact actions this is a reference to the
+#              source channel.
+# TARGET_CHANNEL - the channel which we are promoting to
+# HAB_AUTH_TOKEN - GitHub Auth token used to communicate with the
+#                  Habitat depot and private repos in Chef's GitHub org
+#
 set -eou pipefail
 
-echo "--> Running the promote-hab-pkg.sh script"
+# Export the HAB_AUTH_TOKEN for use of promoting habitat packages to {{TARGET_CHANNEL}}
 HAB_AUTH_TOKEN=$(vault kv get -field auth_token account/static/habitat/chef-ci)
 export HAB_AUTH_TOKEN
 
-source_channel="workstation-build"
- hab pkg promote "${EXPEDITOR_PKG_IDENT}" "${EXPEDITOR_TARGET_CHANNEL}"  "${EXPEDITOR_PKG_TARGET}"
+
+# when this workflow runs, there are env vars that are available to us in the running pod, we are grabbing the source ENV, then assigning it to our next channel
+if [[ "${EXPEDITOR_CHANNEL}" == "unstable" ]]; then
+  echo "This file does not support actions for artifacts promoted to unstable"
+  exit 1
+elif [[ "${EXPEDITOR_CHANNEL}" == "stable" ]]; then
+  export EXPEDITOR_TARGET_CHANNEL="workstation-LTS"
+  echo "My current package is in channel: ${EXPEDITOR_CHANNEL}. I am promoting to ${EXPEDITOR_TARGET_CHANNEL}"
+# elif [[ "${EXPEDITOR_CHANNEL}" == "acceptance" ]]; then
+#   export EXPEDITOR_TARGET_CHANNEL="current"
+#   echo "My current package is in channel: ${EXPEDITOR_CHANNEL}. I am promoting to ${EXPEDITOR_TARGET_CHANNEL}"
+# elif [[ "${EXPEDITOR_CHANNEL}" == "current" ]]; then
+#   export EXPEDITOR_TARGET_CHANNEL="stable"
+#   echo "My current package is in channel: ${EXPEDITOR_CHANNEL}. I am promoting to ${EXPEDITOR_TARGET_CHANNEL}"
+else
+  echo "Unknown EXPEDITOR_CHANNEL: ${EXPEDITOR_CHANNEL}"
+  exit 1
+fi
+
+# Promote the artifacts in Habitat Depot
+  if [[ "${EXPEDITOR_PKG_ORIGIN}" == "core" ]];
+  then
+    echo "Skipping promotion of core origin package ${EXPEDITOR_PKG_ORIGIN}"
+  else
+    echo "Promoting ${EXPEDITOR_PKG_IDENT} to the ${EXPEDITOR_TARGET_CHANNEL} channel"
+    hab pkg promote "${EXPEDITOR_PKG_IDENT}" "${EXPEDITOR_TARGET_CHANNEL}"
+  fi
