6a386e97a4f0530bfd098c06183b7c73491df906
Sean Simmons <sean.simmons@progress.com>
2025-01-03 10:08:48 -0500

    Updating the promote-hab script to grab the correct values out of running env vars
    updated expeditor config to auto promote to the first channel, workstation-build
    
    updated expeditor script with a better name for the staging area
    
    Signed-off-by: Sean Simmons <sean.simmons@progress.com>

--- .expeditor/promote-hab-pkg.sh
+++ .expeditor/promote-hab-pkg.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
 #
-# when using the following workflow, the following env vars are available to us for use.
-
-#   # subscription to fire off any other promotions that you may need for an additional channnel like LTS or something. 
-#   - workload: staged_workload_released:{{agent_id}}:hab_unstable_promote:*
-#     actions:
-#     - bash:.expeditor/scripts/promote-hab-pkgs.sh:
-#         post_commit: true
+# The following standard Expeditor artifact_actions environment variables
+# are automatically set by the calling process:
 #
 # PROMOTABLE - in promotion artifact actions this is a reference to the
 #              source channel.
-# EXPEDITOR_CHANNEL - Read from the expeditor config, this should be the "lowest" channel in your promotion. IE: unstable 
-# EXPEDITOR_TARGET_CHANNEL - the channel which we are promoting to
+# TARGET_CHANNEL - the channel which we are promoting to
 # HAB_AUTH_TOKEN - GitHub Auth token used to communicate with the
 #                  Habitat depot and private repos in Chef's GitHub org
 #
@@ -22,30 +16,25 @@ set -eou pipefail
 HAB_AUTH_TOKEN=$(vault kv get -field auth_token account/static/habitat/chef-ci)
 export HAB_AUTH_TOKEN
 
-
+# # Export the HAB_AUTH_TOKEN for use of promoting habitat packages to {{TARGET_CHANNEL}}
+# HAB_AUTH_TOKEN=$(vault kv get -field auth_token account/static/habitat/chef-ci)
+# export HAB_AUTH_TOKEN
 # when this workflow runs, there are env vars that are available to us in the running pod, we are grabbing the source ENV, then assigning it to our next channel
 if [[ "${EXPEDITOR_CHANNEL}" == "unstable" ]]; then
-  echo "This file does not support actions for artifacts promoted to unstable, that should happen in the /expeditor promote AGENT VERSION command from slack"
+  echo "This file does not support actions for artifacts promoted to unstable"
   exit 1
 elif [[ "${EXPEDITOR_CHANNEL}" == "stable" ]]; then
   export EXPEDITOR_TARGET_CHANNEL="LTS-2024"
-  echo "My current package is in channel: ${EXPEDITOR_CHANNEL}. I am promoting to ${EXPEDITOR_TARGET_CHANNEL}"
-# elif [[ "${EXPEDITOR_CHANNEL}" == "acceptance" ]]; then
-#   export EXPEDITOR_TARGET_CHANNEL="current"
-#   echo "My current package is in channel: ${EXPEDITOR_CHANNEL}. I am promoting to ${EXPEDITOR_TARGET_CHANNEL}"
-# elif [[ "${EXPEDITOR_CHANNEL}" == "current" ]]; then
-#   export EXPEDITOR_TARGET_CHANNEL="stable"
-#   echo "My current package is in channel: ${EXPEDITOR_CHANNEL}. I am promoting to ${EXPEDITOR_TARGET_CHANNEL}"
 else
   echo "Unknown EXPEDITOR_CHANNEL: ${EXPEDITOR_CHANNEL}"
   exit 1
 fi
 
 # Promote the artifacts in Habitat Depot
-  if [[ "${EXPEDITOR_PKG_ORIGIN}" == "core" ]];
-  then
-    echo "Skipping promotion of core origin package ${EXPEDITOR_PKG_ORIGIN}"
-  else
-    echo "Promoting ${EXPEDITOR_PKG_IDENT} to the ${EXPEDITOR_TARGET_CHANNEL} channel"
-    hab pkg promote "${EXPEDITOR_PKG_IDENT}" "${EXPEDITOR_TARGET_CHANNEL}"
-  fi
+if [[ "${EXPEDITOR_PKG_ORIGIN}" == "core" ]];
+then
+  echo "Skipping promotion of core origin package ${EXPEDITOR_PKG_ORIGIN}"
+else
+  echo "Promoting ${EXPEDITOR_PKG_IDENT} to the ${EXPEDITOR_TARGET_CHANNEL} channel"
+  hab pkg promote "${EXPEDITOR_PKG_IDENT}" "${EXPEDITOR_TARGET_CHANNEL}"
+fi
